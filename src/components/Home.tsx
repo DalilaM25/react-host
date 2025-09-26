e2817ae16c3945bc04d859855d13f16813171302
Darya Mikhaylova <melissa25.dm@gmail.com>
2025-09-26 17:29:28 +0300

    fix

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,13 @@
 export const Home = () => {
   return (
     <div className="relative w-full h-auto flex items-center justify-center overflow-hidden rounded-radius my-2">
-      <div 
-        className="absolute inset-0 bg-[url('./assets/png/home.png')] bg-cover bg-center blur-[2px] scale-105"
-      ></div>
+      <div className="absolute inset-0 bg-[url('./assets/png/home.png')] bg-cover bg-center blur-[2px] scale-105"></div>
       <div className="absolute inset-0 bg-black/10"></div>
-      <span className="font-title text-white text-[15rem] relative z-10 drop-shadow-2xl">
+      <span
+        className="font-title text-white text-[15rem] relative z-10
+                 [text-shadow:_0_0_10px_rgb(255_255_255_/_80%),_0_0_20px_rgb(255_255_255_/_60%)]
+                 animate-pulse [animation-duration:4s]"
+      >
         MULTIHUB
       </span>
     </div>
